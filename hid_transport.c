21ac9c68dd013bfd49e54198bc8fdb12d594db4c
mpjpower <mark.power@btinternet.com>
2026-03-31 23:12:02 +0100

    Refactor of i2c to restore functionality

--- hid_transport.c
+++ hid_transport.c
@@ -12,6 +12,8 @@
 
 static uint8_t s_pending_response[REPORT_SIZE] = {0};
 static uint16_t s_pending_response_len = 0;
+static uint8_t s_pending_command[REPORT_SIZE] = {0};
+static uint16_t s_pending_command_len = 0;
 
 static void hid_transport_try_flush(void) {
     if (s_pending_response_len == 0) {
@@ -39,9 +41,17 @@ static void hid_transport_queue_response(const char *response) {
 
 void hid_transport_init(void) {
     s_pending_response_len = 0;
+    s_pending_command_len = 0;
 }
 
 void hid_transport_task(void) {
+    if (s_pending_command_len != 0 && s_pending_response_len == 0) {
+        char response[REPORT_SIZE] = {0};
+        command_interface_process(s_pending_command, s_pending_command_len, response, REPORT_SIZE);
+        s_pending_command_len = 0;
+        hid_transport_queue_response(response);
+    }
+
     hid_transport_try_flush();
 }
 
@@ -50,11 +60,18 @@ void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_
     (void) report_id;
 
     if (report_type == HID_REPORT_TYPE_OUTPUT) {
-        char response[REPORT_SIZE] = {0};
+        uint16_t copy_len = bufsize;
+        if (copy_len > REPORT_SIZE) {
+            copy_len = REPORT_SIZE;
+        }
 
-        command_interface_process(buffer, bufsize, response, REPORT_SIZE);
-        hid_transport_queue_response(response);
-        hid_transport_try_flush();
+        // Keep only one in-flight command. Host should send next command
+        // after reading the previous response.
+        if (s_pending_command_len == 0 && s_pending_response_len == 0) {
+            memset(s_pending_command, 0, sizeof(s_pending_command));
+            memcpy(s_pending_command, buffer, copy_len);
+            s_pending_command_len = copy_len;
+        }
     }
 }
 
